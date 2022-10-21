4fe70a69202ee0ddfb4ca6ed03b3552cb1713ba1
Mayur Kalsekar <69385870+lolg69@users.noreply.github.com>
2022-10-21 20:46:47 +0530

    Add files via upload

---
+++ hangman/hangman.py
@@ -0,0 +1,27 @@
+import random
+attempts = 5
+words = ["nutjob","handjob","jlowbob"]
+word = random.choice(words)
+correct_guessed = []
+while True:
+    if attempts == 0:
+        print("GAME OVER!")
+        break
+    for i in word:
+        if i in correct_guessed:
+            print(i,end=" ")
+        else:
+            print("*",end=" ")
+    print("\n")
+    print("Attempts = " + str(attempts))
+    guess = input("Please enter your guess here.")
+    if guess in correct_guessed:
+        print("This word is already used")
+    elif guess in word:
+        print("yes")
+        correct_guessed.append(guess)
+    else:
+        print("no")
+        correct_guessed.append(guess)
+        attempts-=1
+
